d374a1b3d86b346caa0ee5f9347280134fa19a76
APUNJABI <apunjabi@wisc.edu>
2025-04-06 00:45:22 -0600

    basic working server

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,12 @@
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::io::Write;
+pub mod types;
 
-struct Server{
-    ip: String,
-    listener: Option<TcpListener>
-}
+use crate::types::Server;
 
-impl Server {
-    fn open_connection(&mut self) -> std::io::Result<()>{
-        self.listener = Some(TcpListener::bind(&self.ip).expect("Failed to connect"));
-        println!("Server listening on port 8080...");
-        return Ok(());
-    }
-
-    fn listen_for_connections(&self){
-        for stream in self.listener.as_ref().expect("Listener isn't initialized").incoming(){
-            match stream {
-                Ok(ref socket) => {
-                    println!("New client connected: {}", socket.peer_addr().unwrap());
-                    let response = "Received your message!"; 
-                    stream.unwrap().write_all(response.as_bytes()).expect("Failed to send");            
-                }
-                Err(_) => {
-                    println!("Error accepting connection");
-                }
-            }            
-        }
-    }
-
-    // fn close_connection(&self){
-    // }
-}
 fn main(){
     let mut server = Server {
         ip: String::from("127.0.0.1:8080"),
-        listener: None
+        listener: None,
+        streams: Vec::new()
     };
 
     let _ = server.open_connection();
---
+++ src/types.rs
@@ -0,0 +1,48 @@
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::Write;
+use std::os::windows::io::AsRawSocket;
+use std::net::Shutdown;
+
+pub struct Server{
+    pub ip: String,
+    pub listener: Option<TcpListener>,
+    pub streams: Vec<TcpStream>
+}
+
+impl Server {
+    pub fn open_connection(&mut self) -> std::io::Result<()>{
+        self.listener = Some(TcpListener::bind(&self.ip).expect("Failed to connect"));
+        println!("Server listening on port 8080...");
+        return Ok(());
+    }
+
+    pub fn listen_for_connections(&mut self){
+        for stream in self.listener.as_ref().expect("Listener isn't initialized").incoming(){
+            match stream {
+                Ok(socket) => {
+                    println!("New client connected: {}", socket.peer_addr().unwrap());
+                    self.streams.push(socket);
+                }
+                Err(_) => {
+                    println!("Error accepting connection");
+                }
+            }
+        }
+    }
+
+    pub fn send_response(&self){
+        for mut stream in &self.streams {
+            stream.write_all(b"HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello");
+        }
+    }
+    
+    
+    pub fn close_connection_stream(&self, stream_to_close: &TcpStream){
+        for stream in &self.streams {
+            if stream.as_raw_socket() == stream_to_close.as_raw_socket() {
+                stream.shutdown(Shutdown::Both).expect("shutdown call failed");
+            }
+        }
+    }
+}
